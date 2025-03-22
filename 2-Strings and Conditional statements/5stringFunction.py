e826ed996e09179f06119c81da7d75ab7f31327c
Manas Singh <singhmanas2918@gmail.com>
2025-03-22 22:42:59 +0530

    5 string

---
+++ 2-Strings and Conditional statements/5stringFunction.py
@@ -0,0 +1,9 @@
+str = "i am manas singh student"
+
+#endswith() function
+print(str.endswith("heyyyyyyyy")) # False
+print(str.endswith("student")) # True
+print(str.endswith("t")) # True
+
+#capitalize() function
+print(str.capitalize()) # I am manas singh 
