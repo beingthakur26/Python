5201e6e07c8c55480aa4c71eee3f519059b39d92
Manas Singh <singhmanas2918@gmail.com>
2025-04-05 01:59:13 +0530

    hii

--- 8-Object Oriented Programming System ( OOPS )/6ques_Practice.py
+++ 8-Object Oriented Programming System ( OOPS )/6ques_Practice.py
@@ -1,4 +1,20 @@
 # Question 1.
 # Create student class that takes name & marks of 3 sunjects as arguments in constructor. Then create a method to print the average.
 
+class Student:
+    def __init__(self, name, mark):
+        self.name = name
+        self.mark = mark
 
+    def get_avg(self):
+        sum = 0
+        for val in self.mark:
+            sum += val
+        print("Hii", self.name, "Your avg score is : ", sum/3)
+
+s1 = Student("Tony Stark", [90, 80, 70])
+s1.get_avg()
+
+
+s1.name = "Ironman"
+s1.get_avg()
